6c6f5d713482df85d910b3e488356e4dc2eaa068
ckalgara <chandhukalagara@gmail.com>
2025-11-01 15:23:21 -0500

    chore: Player data model for postgres

--- core/model.go
+++ core/model.go
@@ -1,4 +1,20 @@
 package core
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Player struct {
+	ID        uint    `gorm:"primaryKey;autoIncrement"`
+	UUID      string  `gorm:"type:uuid;default:gen_random_uuid();uniqueIndex"`
+	Name      string  `gorm:"type:varchar(100);not null"`
+	Email     string  `gorm:"type:varchar(100);uniqueIndex;not null"`
+	Age       int     `gorm:"not null"`
+	Team      string  `gorm:"type:varchar(50)"`
+	Score     float64 `gorm:"default:0"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
